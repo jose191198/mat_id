55ff8192257e586de6d100792f401bcda3fc649f
jose191198 <83844671+jose191198@users.noreply.github.com>
2023-05-02 23:09:40 -0600

    Add files via upload

---
+++ arreglos_bid.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Mi primer codigo de arreglos bidireccionales de DataRebels
+
+#1.- Crear una matriz 3x3 con valores de 0 a 8
+import numpy as np
+
+arrbid = np.array([[0,1,2],[3,4,5],[6,7,8]])
+print(type(arrbid))
+print(arrbid)
+                             
+#2.- Crear una matriz identidad de 6x6 utilizando la función .identity().
+mat_id = np.identity(6)
+print(mat_id)
+print(type(mat_id))                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
